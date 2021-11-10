4e35f63c2adfcb4c1d517b4518de5e359d864053
jinyunjae <jinyun3075@naver.com>
2021-11-10 20:53:55 +0900

    LinkedList add

--- src/list/linkedlist/LinkedList.java
+++ src/list/linkedlist/LinkedList.java
@@ -6,7 +6,7 @@ public class LinkedList {
     private Node head;
     private Node tail;
     private int size=0;
-    private class Node{
+    private class Node {
         private Object data;
         private Node next;
         public Node (Object input) {
@@ -15,6 +15,27 @@ public class LinkedList {
         }
         public String toString(){
             return String.valueOf(data);
+        }
+    }
+
+    public void addFirst(Object input){
+        Node newNode= new Node(input);
+        newNode.next = head;
+        head = newNode;
+        size++;
+        if(head.next == null) {
+            tail = head;
+        }
+    }
+
+    public void addLast(Object input) {
+        if(size == 0) {
+            addFirst(input);
+        } else {
+            Node newNode = new Node(input);
+            tail.next = newNode;
+            tail = newNode;
+            size++;
         }
     }
 }
--- src/list/linkedlist/Main.java
+++ src/list/linkedlist/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args){
         LinkedList arr = new LinkedList();
 
+        arr.addFirst(20);
+        arr.addFirst(10);
+
+        arr.addLast(30);
     }
 }
